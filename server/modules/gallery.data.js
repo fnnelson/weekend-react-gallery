4babe69dd969b67e98681262d70dd5eff592c8b6
Forrest Nelson <forrestnnelson@gmail.com>
2023-08-11 20:47:16 -0500

    updated GET to work, updating state, append functional

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/dogger.jpg', description: 'Photo of dog having a good day.', likes: 0 },
+    { id: 3, path: 'images/fancy_fish.jpg', description: 'Photo of a fancy fish.', likes: 0 },
+    { id: 4, path: 'images/gerbil.jpg', description: 'Photo of a pet hamster.', likes: 0 },
+    { id: 5, path: 'images/koala.jpg', description: 'Photo of Koala D. Time.', likes: 0 },
+    { id: 6, path: 'images/smiling_goat.jpg', description: 'Photo of a goated goat.', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,43 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  const getGallery = () => {
+  let [artGalleryList, setArtGalleryList] = useState([]);
 
+  const getGallery = () => {
+    axios.get('/gallery')
+      .then(response => {
+        console.log("response.data received in GET /gallery", response.data)
+        setArtGalleryList(response.data)
+      })
+      .catch(error => {
+        console.log("error with GET", error)
+      })
   }
 
+  useEffect(() => {
+    getGallery();
+  }, [])
 
 
-
-
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <div className="art-gallery">
+        {artGalleryList.map(art => (
+          <div key={art.id}>
+            <img src={art.path} alt={art.description}/>
+            <p>{art.description}</p>
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
